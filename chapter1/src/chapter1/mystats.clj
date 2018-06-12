973879a240560434f60f92d632a6bfaa4e11f65d
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-06-12 06:13:34 +0200

    further functionality in mystats

--- chapter1/src/chapter1/mystats.clj
+++ chapter1/src/chapter1/mystats.clj
@@ -6,3 +6,43 @@
   (/ (apply + xs) (count xs))
 )
 
+
+;;slow O(nlogn) and only for count >=2
+(defn median
+   [xs]
+   (let [sorted (sort xs)
+         len    (count xs)
+         middle (quot len 2)]
+       (if (odd? len)
+             (nth sorted middle)
+             (mean [(nth sorted middle) (nth sorted (inc middle))])
+       )
+   )
+)
+
+(defn square
+    [x]
+    (* x x)
+)
+
+(defn variance
+   [xs]
+   (let [x-bar (mean xs)]
+      (mean (map #(square (- % x-bar)) xs))
+   )     
+)
+
+(defn standard-deviation
+   [xs]
+   (Math/sqrt (variance xs))  
+)
+
+
+;;slow O(nlogn)
+(defn quantile
+  [xs q]
+  (let [n (count xs)
+        i (int (* (dec n) q))]
+        (nth (sort xs) i)
+   )
+)
--- chapter1/test/chapter1/mystats_test.clj
+++ chapter1/test/chapter1/mystats_test.clj
@@ -2,8 +2,70 @@
   (:require [clojure.test :refer :all]
             [chapter1.mystats :as ms]))
 
-(deftest a-test
+(deftest test-mean
   (testing "mean"
     (is (ms/mean [0 1 2]) 1)
   )
 )
+
+
+(deftest test-median-odd
+  (testing "median odd"
+    (is (ms/median [0 1 2]) 1)
+  )
+)
+
+(deftest test-median-even
+  (testing "median even"
+    (is (ms/median [0 2 4 5]) 3)
+  )
+)
+
+
+(deftest test-variance-one
+  (testing "variance of one element is zero"
+    (is (ms/variance [6]) 0)
+  )
+)
+
+(deftest test-variance
+  (testing "variance"
+    (is (ms/variance [0 4]) 4)
+  )
+)
+
+
+(deftest test-sd
+  (testing "standard-deviation"
+    (is (ms/standard-deviation [0 4]) 2)
+  )
+)
+
+(deftest test-quantile-0
+  (testing "quatile"
+    (is (ms/quantile [0 1 2 4] 0) 0)
+  )
+)
+
+(deftest test-quantile-0.25
+  (testing "quatile"
+    (is (ms/quantile [0 1 2 4] 0.25) 1)
+  )
+)
+
+(deftest test-quantile-0.5
+  (testing "quatile"
+    (is (ms/quantile [0 1 2 4] 0.75) 2)
+  )
+)
+
+(deftest test-quantile-1
+  (testing "quatile"
+    (is (ms/quantile [0 1 2 4] 1) 4)
+  )
+)
+
+
+
+
+
